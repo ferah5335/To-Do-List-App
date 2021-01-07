9eec8965a4f3f0b0042dbecf478e8e5e2a967f62
Muhammet Selim Ferah <muhammetselimferah@hotmail.com>
2021-01-07 17:27:35 +0300

    javaScript added

---
+++ main.js
@@ -0,0 +1,21 @@
+const button = document.getElementById('add');
+const container = document.getElementById('toDoContainer');
+const input = document.getElementById('input');
+
+button.addEventListener('click', function(){
+    var paragraf = document.createElement('P');
+    paragraf.classList.add('paragraf-styling')
+    paragraf.innerText = input.value;
+    container.appendChild(paragraf);
+    input.value='';
+
+    paragraf.addEventListener('click', function(){
+        paragraf.style.textDecoration ='line-through'
+
+    })
+
+    paragraf.addEventListener('dblclick', function(){
+        container.removeChild(paragraf)
+
+    })
+})
